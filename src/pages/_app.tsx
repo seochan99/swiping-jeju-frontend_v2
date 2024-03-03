e10385f480f28b4a19891ea14ce3dfe4072e11c8
Jeongmin Lee <clcl6084@gmail.com>
2024-03-04 03:28:20 +0900

    ⚙️ Settings: initial set-up for styled-component theme and fonts, break points, reset CSS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css';
+import { GlobalStyles } from '@/styles/Global';
+import { theme } from '@/styles/Theme';
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Component {...pageProps} />;
+    </ThemeProvider>
+  );
 }
