af68cb742e2d027c83a8bd7baa784197be727a38
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2024-03-09 15:36:03 +0900

    ✨Feat: Home page - 2type(landing, map)

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,60 @@
-import React from 'react';
+'use client';
 
-const HomePage = () => {
-  return <div>HomePage</div>;
-};
+import React, { useEffect, useState } from 'react';
 
-export default HomePage;
+import HomeLanding from '@/components/home/HomeLanding';
+import HomeMapSelection from '@/components/home/HomeMapSelection';
+import { SwipingAlbum } from '@/interfaces/home/home';
+
+function Home(): JSX.Element {
+  const [step, setStep] = useState<number>(0);
+  const [animate, setAnimate] = useState<boolean>(false);
+  const [swipingAlbum, setSwipingAlbum] = useState<SwipingAlbum>({
+    title: '',
+    mapList: [],
+  });
+
+  console.log(swipingAlbum);
+  // 스텝 변경시 애니메이션 효과
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setAnimate(false);
+    }, 1000);
+    setAnimate(true);
+    return () => clearTimeout(timeoutId);
+  }, [step]);
+
+  // 다음 스텝으로 넘어가는 함수
+  const handleNext = (): void => {
+    setStep((prevStep) => prevStep + 1);
+  };
+
+  // 처음 접속시 로컬스토리지에 isFirst 값이 없으면 true로 설정
+  useEffect(() => {
+    if (localStorage.getItem('isFirst') === null) {
+      localStorage.setItem('isFirst', 'true');
+    }
+  }, []);
+
+  return (
+    <>
+      <div className={`mt-12 ${animate ? 'animate-fadeIn' : ''} bg-[#050014]`}>
+        {step === 0 && <HomeLanding onNext={handleNext} />}
+        {step === 1 && (
+          <HomeMapSelection
+            onNext={handleNext}
+            setSwipingAlbum={setSwipingAlbum}
+          />
+        )}
+        {/* {step === 2 && (
+          <HomeStyleInput
+            swipingAlbum={swipingAlbum}
+            setSwipingAlbum={setSwipingAlbum}
+          />
+        )} */}
+      </div>
+    </>
+  );
+}
+
+export default Home;
